2ffcc89b2e76b2d9d5bbeb38749f9bb6a21cdab0
Thelsandro <thelsandro.antunes@gmail.com>
2018-12-19 19:04:46 -0400

    3ª Versão do Banco de Dados
    Criação de novas tabelas e inserção de registros

--- Loja Virtual/Database/mydb.sql
+++ Loja Virtual/Database/mydb.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 19, 2017 at 11:50 AM
--- Server version: 5.7.14
--- PHP Version: 7.0.10
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 19, 2018 at 11:03 PM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,11 +25,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE IF NOT EXISTS `message` (
+  `mid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `mobile` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mssg` text NOT NULL,
+  `cdate` date NOT NULL,
+  PRIMARY KEY (`mid`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `message`
+--
+
+INSERT INTO `message` (`mid`, `name`, `mobile`, `email`, `mssg`, `cdate`) VALUES
+(3, 'Judas', '+5592991408932', 'judas@gmail.com', 'tem gente que ganha respeito de judas no quesito de traiÃ§Ã£o.', '2018-12-19');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `ordrs`
 --
 
-CREATE TABLE `ordrs` (
-  `myid` int(11) NOT NULL,
+DROP TABLE IF EXISTS `ordrs`;
+CREATE TABLE IF NOT EXISTS `ordrs` (
+  `myid` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) NOT NULL,
   `img` varchar(50) NOT NULL,
   `name` varchar(20) NOT NULL,
@@ -37,25 +64,28 @@ CREATE TABLE `ordrs` (
   `ordr` varchar(200) NOT NULL,
   `pr` varchar(20) NOT NULL,
   `sts` varchar(20) NOT NULL,
-  `cdate` date NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `cdate` date NOT NULL,
+  PRIMARY KEY (`myid`)
+) ENGINE=MyISAM AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `ordrs`
 --
 
 INSERT INTO `ordrs` (`myid`, `uid`, `img`, `name`, `mobile`, `email`, `addr`, `ordr`, `pr`, `sts`, `cdate`) VALUES
-(28, 3, 'img/.jpg', 'dddd', '+4354354', 'dd@gmail.com', 'sdsdefd', 'Photoshop', '$100', 'Pending', '2017-10-16'),
-(27, 3, 'img/11.jpg', 'dd done', '+64143434', 'dd@gmail.com', 'dd new order Taxes America', 'CSS ', '$200', 'Dispatched', '2017-10-16'),
-(26, 2, 'img/11.jpg', 'David ', '+374341', 'david@gmail.com', 'My new NY Address David America', 'CSS ', '$200', 'Dispatched', '2017-10-16'),
-(29, 3, 'img/.jpg', 'dsdasd', '+54645', 'dd', 'sdasdsd', 'Photoshop', '$100', 'Pending', '2017-10-16'),
-(25, 2, 'img/.jpg', 'David', '+75435434', 'david@gmail.com', 'dfsdde David NY', 'Photoshop', '$100', 'Dispatched', '2017-10-16'),
-(30, 2, 'img/.jpg', 'rrrr', '+14314543', 'jj@gmail.com', 'jj Ny City ', 'Photoshop', '$100', 'Pending', '2017-10-19'),
-(31, 2, 'img/11.jpg', 'rr', 'rr', 'rr', 'rrr', 'CSS ', '$200', 'Pending', '2017-10-19'),
-(32, 2, 'img/.jpg', 'jj', '+5435413', 'jj@example.com', 'fddsfsdf', 'Photoshop', '$100', 'Pending', '2017-10-19'),
-(33, 2, 'img/.jpg', 'jj', '+41354134', 'jj@example.com', 'sdahksahd', 'Photoshop', '$100', 'Pending', '2017-10-19'),
-(34, 2, 'img/16.jpg', 'jj', '+54135441', 'jj@example.com', 'fdfdsffs', 'Blogger', '$200', 'Pending', '2017-10-19'),
-(35, 1, 'img/15.jpg', 'admin', '+34414', 'admin@example.com', 'customerbehalf link access ', 'PHP ', '$400', 'Pending', '2017-10-19');
+(28, 3, 'images/99.jpg', 'dddd', '+4354354', 'dd@gmail.com', 'sdsdefd', 'Photoshop', '$100', 'Pendente', '2017-10-16'),
+(27, 3, 'images/11.jpg', 'dd done', '+64143434', 'dd@gmail.com', 'dd new order Taxes America', 'CSS ', '$200', 'Despachado', '2017-10-16'),
+(26, 2, 'images/11.jpg', 'David ', '+374341', 'david@gmail.com', 'My new NY Address David America', 'CSS ', '$200', 'Despachado', '2017-10-16'),
+(42, 4, 'images/23.jpg', 'thelsandro', '', 'thelsandro.antunes@gmail.com', '', 'Advanced', 'R$500', 'Pending', '2018-12-19'),
+(31, 2, 'images/11.jpg', 'rr', 'rr', 'rr', 'rrr', 'CSS ', '$200', 'Pendente', '2017-10-19'),
+(34, 2, 'images/16.jpg', 'jj', '+54135441', 'jj@example.com', 'fdfdsffs', 'Blogger', '$200', 'Pendente', '2017-10-19'),
+(35, 1, 'images/15.jpg', 'admin', '+55555555', 'admin@example.com', 'customerbehalf link access ', 'PHP ', '$400', 'Dispatched', '2017-10-19'),
+(36, 4, 'images/01.jpg', 'thelsandro', '+559839483989', 'thelsandro.antunes@gmail.com', '', '1808', 'R$45', 'Dispatched', '2018-12-18'),
+(37, 4, 'images/01.jpg', 'thelsandro', '', 'thelsandro.antunes@gmail.com', '', '1808', 'R$45', 'Pendente', '2018-12-18'),
+(38, 4, 'images/25.jpg', 'thelsandro', '', 'thelsandro.antunes@gmail.com', '', 'Ad HTML ', 'R$300', 'Pendente', '2018-12-18'),
+(39, 4, 'images/23.jpg', 'thelsandro', '', 'thelsandro.antunes@gmail.com', '', 'Advanced', 'R$500', 'Despachado', '2018-12-18'),
+(40, 4, 'images/07.jpg', 'thelsandro', '+559839483989', 'thelsandro.antunes@gmail.com', '', 'O Rei do Inverno', 'R$43', 'Cancelled', '2018-12-19'),
+(41, 4, 'images/04.jpg', 'thelsandro', '+551187871212', 'thelsandro.antunes@gmail.com', 'TARUMÃƒ', 'O Jardim das AfliÃ§Ãµes', 'R$89', 'Pending', '2018-12-19');
 
 -- --------------------------------------------------------
 
@@ -63,29 +93,40 @@ INSERT INTO `ordrs` (`myid`, `uid`, `img`, `name`, `mobile`, `email`, `addr`, `o
 -- Table structure for table `product`
 --
 
-CREATE TABLE `product` (
-  `pid` int(11) NOT NULL,
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE IF NOT EXISTS `product` (
+  `pid` int(11) NOT NULL AUTO_INCREMENT,
   `img` varchar(50) NOT NULL,
   `name` varchar(150) NOT NULL,
   `des` varchar(150) NOT NULL,
   `pr` varchar(20) NOT NULL,
-  `cdate` varchar(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `cdate` varchar(20) NOT NULL,
+  PRIMARY KEY (`pid`)
+) ENGINE=MyISAM AUTO_INCREMENT=46 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product`
 --
 
 INSERT INTO `product` (`pid`, `img`, `name`, `des`, `pr`, `cdate`) VALUES
-(23, 'img/18.jpg', 'New Product', 'sdasdsadasd', '43334', '2017-10-16'),
-(15, 'img/14.jpg', 'JavaScript', 'best JS courses', '$200', '2017-10-16'),
-(16, 'img/15.jpg', 'PHP ', 'PHP training courses', '$400', '2017-10-16'),
-(13, 'img/12.jpg', 'HTML Course', 'best courses in urdu Hindi', '$200', '2017-10-16'),
-(18, 'img/16.jpg', 'Blogger', 'Blogger course', '$200', '2017-10-16'),
-(12, 'img/11.jpg', 'CSS ', 'Learn Cascading Style with project in Urdu Hidni', '$200', '2017-10-16'),
-(11, 'img/.jpg', 'Photoshop', 'Best Training courses in Urdu Hindi language. ', '$100', '2017-10-16'),
-(25, 'img/23.jpg', 'Advanced', 'JavaScrip Advanced Curses', '$500', '2017-10-19'),
-(26, 'img/25.jpg', 'Ad HTML ', 'Learn new HTML course', '$300', '2017-10-19');
+(23, 'images/18.jpg', 'novo produto', 'teste', 'R$ 400', '2017-10-16'),
+(15, 'images/14.jpg', 'JavaScript', 'best JS courses', 'R$200', '2017-19-12'),
+(16, 'images/15.jpg', 'PHP ', 'PHP training courses', 'R$210', '2018-12-19'),
+(13, 'images/12.jpg', 'HTML Course', 'best courses in urdu Hindi', 'R$200', '2017-10-16'),
+(18, 'images/16.jpg', 'Blogger', 'Blogger course', 'R$250', '2017-10-16'),
+(12, 'images/11.jpg', 'CSS ', 'Learn Cascading Style with project in Urdu Hidni', 'R$170', '2018-12-19'),
+(25, 'images/23.jpg', 'Advanced', 'JavaScrip Advanced Curses', 'R$500', '2017-10-19'),
+(26, 'images/25.jpg', 'Ad HTML ', 'Learn new HTML course', 'R$300', '2017-10-19'),
+(28, 'images/01.jpg', '1808', 'Autor: Laurentino Gomes', 'R$45', '2018-12-19'),
+(30, 'images/02.jpg', '1822', 'Laurentino Gomes', 'R$43', ''),
+(32, 'images/03.jpg', '1889', 'Laurentino Gomes', 'R$47', ''),
+(34, 'images/04.jpg', 'O Jardim das AfliÃ§Ãµes', 'Olavo de Carvalho', 'R$89', '2018-19-12'),
+(36, 'images/05.jpg', 'O Imbecil Coletivo', 'Olavo de Carvalho', 'R$85', ''),
+(37, 'images/06.jpg', 'O Mínimo que você precisa saber para não ser um Idiota', 'Olavo de Carvalho', 'R$115', ''),
+(39, 'images/06.jpg', 'Excalibur', 'Bernard Cornwell', 'R$42', ''),
+(41, 'images/07.jpg', 'O Rei do Inverno', 'Bernard Cornwell', 'R$43', ''),
+(43, 'images/08.jpg', 'O Inimigo de Deus', 'Bernard Cornwell', 'R$49', ''),
+(45, 'images/43.jpg', 'DragÃ£os de Ã‰ter', 'Rafael Draccon', 'R$ 37', '2018-12-19');
 
 -- --------------------------------------------------------
 
@@ -93,13 +134,15 @@ INSERT INTO `product` (`pid`, `img`, `name`, `des`, `pr`, `cdate`) VALUES
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(15) NOT NULL,
   `user_email` varchar(40) NOT NULL,
   `user_pass` varchar(255) NOT NULL,
-  `joining_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `joining_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -107,50 +150,9 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_pass`, `joining_date`) VALUES
 (1, 'admin', 'admin@example.com', '$2y$10$ujej4UpRJsbf3ETJRrqg8uQ7pBv4HX5w77dho8cD9t8zVK/zt75Na', '2017-10-15 11:33:28'),
-(2, 'jj', 'jj@example.com', '$2y$10$aQUx3BOwroeFTAWja6ThF.H5y99jgike6o/IskgIlxo.7QLUHB7.C', '2017-10-15 11:33:48'),
-(3, 'dd', 'dd@gmail.com', '$2y$10$KR3HabnOr3dgusF4XWJnQ.jnVcNfl8adjN4onmTr66/y045MCCyam', '2017-10-16 07:19:25');
+(4, 'thelsandro', 'thelsandro.antunes@gmail.com', '$2y$10$oRC4FqM9vNHQliWZuRQGBuMpSh8bzvmMysncSPK8Gqo5cfY2rem9W', '2018-12-17 19:23:34');
+COMMIT;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `ordrs`
---
-ALTER TABLE `ordrs`
-  ADD PRIMARY KEY (`myid`);
-
---
--- Indexes for table `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`pid`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `ordrs`
---
-ALTER TABLE `ordrs`
-  MODIFY `myid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
---
--- AUTO_INCREMENT for table `product`
---
-ALTER TABLE `product`
-  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
