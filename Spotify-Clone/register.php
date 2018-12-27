bb2f9d5b708257b0ade05a72aad15566cf89e8fb
Thelsandro <thelsandro.antunes@gmail.com>
2018-12-27 17:38:37 -0400

    Atualização de Registro LOGIN
    Cadastro de novo usuário

--- Spotify-Clone/register.php
+++ Spotify-Clone/register.php
@@ -1,3 +1,58 @@
+<?php
+	
+	function sanitizeFormPassword($inputText)
+	{
+		$inputText = strip_tags($inputText);
+
+		return $inputText;
+	}
+
+	function sanitizeFormUsername($inputText)
+	{
+		$inputText = strip_tags($inputText);
+		$inputText = str_replace("", "", $inputText);
+
+		return $inputText;
+	}
+
+	function sanitizeFormString($inputText)
+	{
+		$inputText = strip_tags($inputText);
+		$inputText = str_replace("", "", $inputText);
+		$inputText = ucfirst(strtolower($inputText));
+
+		return $inputText;
+	}
+
+	if (isset($_POST['loginButton'])) 
+	{
+		//echo "O botão Login foi pressionado";
+	}
+
+	if (isset($_POST['registerButton'])) 
+	{
+		//echo "O botão Login foi pressionado";
+		$username = sanitizeFormUsername($_POST['username']);
+		//$username = strip_tags($username);
+		//$username = str_replace("", "", $username);
+
+		$firstName = sanitizeFormString($_POST['firstName']);
+		//$firstName = strip_tags($firstName);
+		//$firstName = str_replace("", "", $firstName);		
+		//$firstName = ucfirst(strtolower($firstName));
+
+		$lastName = sanitizeFormString($_POST['lastName']);
+		//$firstName = strip_tags($firstName);
+		//$firstName = str_replace("", "", $firstName);		
+		//$firstName = ucfirst(strtolower($firstName));
+		$email = sanitizeFormString($_POST['email']);
+		$email2 = sanitizeFormString($_POST['email2']);
+
+		$password = sanitizeFormPassword($_POST['password']);
+		$password2 = sanitizeFormPassword($_POST['password2']);
+	}
+?>
+
 <html>
 <head>
 	<title>Bem-vindo ao Spotify-CLone</title>
@@ -9,7 +64,7 @@
 			<h2>Login para sua conta</h2>
 			<p>
 				<label for="loginUsername">Usuário</label>
-				<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. thelsandorAntunes" requerid>
+				<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. zéCarioca" requerid>
 			</p>
 			<p>
 				<label for="loginPassword">Senha</label>
@@ -23,23 +78,23 @@
 			<h2>Crie sua conta gratuitamente</h2>
 			<p>
 				<label for="username">Usuário</label>
-				<input id="username" name="username" type="text" placeholder="e.g. thelsandorAntunes" requerid>
+				<input id="username" name="username" type="text" placeholder="e.g. zéCarioca" requerid>
 			</p>
 			<p>
 				<label for="firstName">Nome</label>
-				<input id="firstName" name="firstName" type="text" placeholder="e.g. Thelsandro" requerid>
+				<input id="firstName" name="firstName" type="text" placeholder="e.g. Zé" requerid>
 			</p>
 			<p>
 				<label for="lastName">Sobrenome</label>
-				<input id="lastName" name="lastName" type="text" placeholder="e.g. Antunes" requerid>
+				<input id="lastName" name="lastName" type="text" placeholder="e.g. Carioca" requerid>
 			</p>
 			<p>
 				<label for="email">Email</label>
-				<input id="email" name="email" type="text" placeholder="e.g. thelsandro@gmail.com" requerid>
+				<input id="email" name="email" type="text" placeholder="e.g. ze@gmail.com" requerid>
 			</p>
 			<p>
 				<label for="email2">Confirmar Email</label>
-				<input id="email2" name="email2 " type="text" placeholder="e.g. thelsandro@gmail.com" requerid>
+				<input id="email2" name="email2 " type="text" placeholder="e.g. ze@gmail.com" requerid>
 			</p>
 			<p>
 				<label for="password">Senha</label>
