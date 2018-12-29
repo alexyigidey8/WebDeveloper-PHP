2cdc8ce19550fc867aff19f42d9c50f9d93ddcae
Thelsandro <thelsandro.antunes@gmail.com>
2018-12-29 18:58:43 -0400

    3ª Versão Spotify-Clone em PHP
    Novos ícones, imagens e adição de mp3.

---
+++ Spotify-Clone/includes/header.php
@@ -0,0 +1,34 @@
+<?php
+
+	include("includes/config.php");
+
+	//session_destroy(); LOGOUT
+
+	if ($_SESSION['userLoggedIn']) 
+	{
+		$userLoggedIn = $_SESSION['userLoggedIn'];
+	}
+	else
+	{
+		header("Location: register.php");
+	}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bem-vindo ao Spotify-Clone </title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+
+<body>
+	
+
+	<div id="mainContainer">
+
+		<div id="topContainer">
+			<?php include("includes/navBarContainer.php")?>
+
+			<div id="mainViewContainer">
+				<div id="mainContent">
