015cc8049880ec173bd1cd6ddbf9c32031b212d1
Thelsandro <thelsandro.antunes@gmail.com>
2019-01-03 18:47:53 -0400

    1ª Versão Google-Clone em PHP

--- Google-Clone/index.php
+++ Google-Clone/index.php
@@ -11,7 +11,7 @@
 		<div class="mainSection">
 	
 			<div class="logoContainer">
-				<img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+				<img src="assets/images/thelsandro_google.png">
 			</div>
 
 			<div class="searchContainer">
