9541e9cb0db56460fbfa89a4db6a7f2462b5f540
Thelsandro <thelsandro.antunes@gmail.com>
2019-01-03 18:25:25 -0400

    Versão 0  Google-Clone em PHP
    Iniciando o projeto.

---
+++ Google-Clone/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bem-vindo ao </title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+	<div class="wrapper indexPage">
+		
+		<div class="mainSection">
+	
+			<div class="logoContainer">
+				<img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+			</div>
+
+			<div class="searchContainer">
+
+				<form action="search.php" method="get">
+					<input class="searchBox" type="text" name="term" placeholder="Digite sua pesquisa...">
+					<input class="searchButton" type="submit" value="Pesquise">	
+				</form>
+			</div>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
